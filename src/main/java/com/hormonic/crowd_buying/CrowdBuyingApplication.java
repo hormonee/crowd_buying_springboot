816ca20460e19203d1f5b8db9f437606a969af2b
hormonee <mmmobl@naver.com>
2024-02-26 18:54:19 +0900

    240226 Crowd Buying Project(Server) created

---
+++ src/main/java/com/hormonic/crowd_buying/CrowdBuyingApplication.java
@@ -0,0 +1,13 @@
+package com.hormonic.crowd_buying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdBuyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdBuyingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hormonic/crowd_buying/CrowdBuyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hormonic.crowd_buying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdBuyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
