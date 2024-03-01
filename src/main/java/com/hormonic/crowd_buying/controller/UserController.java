33b413c59a6a45c3d8e978e672882fa7936eef86
hormonee <mmmobl@naver.com>
2024-03-01 23:40:25 +0900

    jpa practice

--- src/main/java/com/hormonic/crowd_buying/controller/UserController.java
+++ src/main/java/com/hormonic/crowd_buying/controller/UserController.java
@@ -1,4 +1,59 @@
 package com.hormonic.crowd_buying.controller;
 
+import com.hormonic.crowd_buying.domain.entity.User;
+import com.hormonic.crowd_buying.service.user.UserService;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getUserList() {
+        return userService.getUserList();
+    }
+
+    @GetMapping("/name/{name}")
+    public User getUserByName(@PathVariable("name") String userName) {
+        return userService.getUserByName(userName);
+    }
+
+    @GetMapping("/email/{email}")
+    public User getUserByEmail(@PathVariable("email") String userEmail) {
+        return userService.getUserByEmail(userEmail);
+    }
+
+    @GetMapping("/filter/{component}")
+    public List<User> getUserNameByComponent(@PathVariable("component") String component) {
+        return userService.getUserListByComponent(component);
+    }
+
+    @GetMapping("/custom/{element}")
+    public User testCustomizedQueryMethod(@PathVariable("element") String element) {
+        return userService.testCustomizedMethod(element);
+    }
+
+    @GetMapping("/custom2/{element}")
+    public List<User> testCustomizedQueryMethod2(@PathVariable("element") String element) {
+        return userService.testCustomizedMethod2(element);
+    }
+
+    @PostMapping()
+    public User createUser() {
+        User newUser = User.builder()
+                .userName("kim2")
+                .userEmail("kim2@gmail.com")
+                .build();
+        return userService.createUser(newUser);
+    }
+
 }
--- src/main/java/com/hormonic/crowd_buying/repository/UserRepository.java
+++ src/main/java/com/hormonic/crowd_buying/repository/UserRepository.java
@@ -1,4 +1,31 @@
 package com.hormonic.crowd_buying.repository;
 
-public interface UserRepository {
+import com.hormonic.crowd_buying.domain.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    List<User> findAll();
+
+    User findByUserName(String userName);
+
+    User findByUserEmail(String userEmail);
+
+    List<User> findAllByUserNameLike(String component);
+//    List<User> findAllByUserNameLike(String component, Pageable pageable);
+
+    @Query("select u from User u where u.userName = :element")
+    User findByCustomizedMethod(@Param("element") String element);
+
+    @Query("select u from User u where u.userEmail like :element")
+    List<User> findByCustomizedMethod2(@Param("element") String element);
+
 }
